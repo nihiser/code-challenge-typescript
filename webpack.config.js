57d37c43d119ac8fffcfa6e1900fcbab0055fb73
Matt Nihiser <mattnihiser@gmail.com>
2019-11-14 12:24:13 -0500

    Button Component, Webpack updates, Build process (#1)
    * setup webpack for css-modules, autoprefixer. updated build process, Button as test component
    
    * updated readme with build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,76 @@
+const path =  require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
   mode: "production",
 
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
-
-  resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".tsx"]
+  entry: {
+    index: ["./src/index.tsx"],
   },
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
+    publicPath: "/"
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false
+    })
+  ],
+
+  devtool: 'source-map',
+
+  resolve: { extensions: [".tsx", ".js", ".css", ".scss"] },
 
   module: {
     rules: [
       {
         test: /\.ts(x?)$/,
         exclude: /node_modules/,
-        use: [
-          {
-            loader: "ts-loader"
-          }
-        ]
+        use: [{ loader: "ts-loader" }]
       },
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       {
         enforce: "pre",
         test: /\.js$/,
         loader: "source-map-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: './dist/'
+            }
+          },
+          {
+            loader: 'css-loader', 
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]--[hash:base64:5]'
+              }
+            }
+          },
+          {loader: 'postcss-loader'}
+        ]
+      },
+      {
+        test: /\.scss$/, 
+        use: [
+          {loader: "style-loader"},
+          {loader: "css-modules-typescript-loader"},
+          {loader: "css-loader", options: {modules: true}},
+          {loader: "sass-loader"}
+        ]
       }
     ]
   },
@@ -34,7 +80,7 @@ module.exports = {
   // This is important because it allows us to avoid bundling all of our
   // dependencies, which allows browsers to cache those libraries between builds.
   externals: {
-    react: "React",
+    "react": "React",
     "react-dom": "ReactDOM"
   }
 };
